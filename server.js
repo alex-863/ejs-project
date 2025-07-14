f9dc858558858e9cf85a9cc416c0021571eca683
alex-863 <alexcode863@gmail.com>
2025-07-14 10:36:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const path = require("path");
+const data = require("./MOCK_DATA.json");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use((req, res) => {
-  res.render("index");
+  //   res.send(path.join(__dirname, "views", "index.html"));
+  res.json(data);
 });
 
 let PORT = process.env.PORT || 8000;
